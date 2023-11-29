3c251b6360e64c606ac01eee76e80adeddf25b76
Daniel <126022409+DanielGonzalezGil@users.noreply.github.com>
2023-11-28 23:25:00 -0500

    updated code

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+# Balldontlie API base URL
+BALLO_DONT_LIE_API_BASE = "https://www.balldontlie.io/api/v1/"
 
 
 # Simulated API endpoint
 @app.route("/api/data", methods=["GET"])
 def get_api_data():
-    # Replace this with actual API call or data retrieval logic
-    data = [
-        {"id": 1, "name": "Item 1"},
-        {"id": 2, "name": "Item 2"},
-        {"id": 3, "name": "Item 3"},
-    ]
-    return jsonify(data)
+    # Get the player name from the request parameters
+    player_name = request.args.get("player_name")
+
+    if not player_name:
+        return jsonify({"error": "Player name is required"}), 400
+
+    # Construct the API endpoint for player search
+    player_search_endpoint = f"{BALLO_DONT_LIE_API_BASE}players"
+    params = {"search": player_name}
+
+    # Make the API call
+    response = requests.get(player_search_endpoint, params=params)
+    print(response.text)
+
+    if response.status_code == 200:
+        # Extract relevant player data
+        player_data = response.json().get("data", [])
+
+        return jsonify(player_data)
+    else:
+        return jsonify({"error": "Error fetching player data"}), response.status_code
 
 
 # Route for the home page
